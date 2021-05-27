2dd9223e74d402b59bc1cbc8040e324524e71e30
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-05-27 22:39:43 +0545

    Removed the comment

--- translator.py
+++ translator.py
@@ -1,11 +1,11 @@
 phrase = input("enter a phrase: ")
 new = ""
-'''for letter in phrase:
+for letter in phrase:
     if letter in "AEIOUaeiou":
         new = new + "g"
     else:
         new = new + letter
-print(new)'''
+print(new)
 #for both lower and uppercase
 
 for letter in phrase:
