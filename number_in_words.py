2a04b2ebebfdbb7585cc2ea72ace60b082c411ec
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-05-30 11:24:21 +0545

    Use list instead of string and add integer division

--- number_in_words.py
+++ number_in_words.py
@@ -1,20 +1,22 @@
 def number_in_words(num):
     number_words_0 = {
-        "1" : "One",
-        "2" : "Two",
-        "3" : "Three"
+        "1" : "one",
+        "2" : "two",
+        "3" : "three",
     }
     number_words_1 = {
         "2" : "Twenty",
         "3" : "Thirty",
         "4" : "Forty"
     }
-    digit = ""
+    digit = [0, 0]
     temp_num = num
     for i in range(2):
         digit[i] = str(temp_num % 10)
-        temp_num = temp_num / 10
-    return str(number_words_1.get(digit[1])) + " " + str(number_words_0.get(digit[0]))
+        print (digit[i])
+        temp_num = temp_num // 10
+    return str(number_words_1.get(digit[1])) + "" + str(number_words_0.get(digit[0]))
 
-print(number_in_words(22))    
-        
+
+
+print(number_in_words(31))        
