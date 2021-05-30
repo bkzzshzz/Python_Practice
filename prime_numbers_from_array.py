9a3c02695dd0c103471b45b93a1d0a82267c06fa
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-05-30 11:27:33 +0545

    Use append in lieu of index

--- prime_numbers_from_array.py
+++ prime_numbers_from_array.py
@@ -1,11 +1,15 @@
+def check_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    
+    return True
+
 list_numbers = [2, 5, 7, 9]
 primes  = []
-for i in range(len(list_numbers)):
-    for j in range(2, list_numbers[i]):
-            if (list_numbers[i] % j) == 0:
-                break
-    primes[i] = list_numbers[i]
 
-print(primes)
-                
-        
+for number in list_numbers:
+    if check_prime(number):
+        primes.append(number)
+
+print(f'The list of primes is:{primes}')
