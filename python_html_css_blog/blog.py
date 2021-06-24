c86e6a140fb9d37522c8159c9cd5a10d39e55809
scimad <scimad2.71@gmail.com>
2021-06-24 15:41:15 +0545

    Made dynamic listing of posts working

--- python_html_css_blog/blog.py
+++ python_html_css_blog/blog.py
@@ -2,42 +2,39 @@ import http.server
 import socketserver
 import os
 
+PORT = 8001
 class blog_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         
         if self.path == '/':
-            self.path = 'blog.html'
-        elif self.path == "/home.html":
+            self.path = '/home.html'
+
+        if self.path == "/home.html":
             file_list = os.listdir("./blog-posts")
-            template = open("home.template.html", "r").read()
-            new_file = open("home.html","w").write(template)
+            template_string = open("home.template.html", "r").read()
             
             
-            for i in range(len(file_list)):
-                print(i)
-                link_list = "<li><a href=\"{filename" + str(i) +"}\">{filename" + str(i) + "}</li>\n{variable}\n"
-                print(link_list)
-                new_file = open("home.html").read().format(variable = link_list)
-                print(new_file)
-                open("home.html", "w").write(new_file)
+            link_for_single_file = '''
+            <li><a href="blog-posts/{file_name}">{file_title}</a></li>
+            '''
+            list_of_links = '''
+            '''
+            for name_of_file in file_list:
+                # Following code extracts title form <title> -------- </title> section of each file
+                file_handle = open("./blog-posts/" + name_of_file)
+                file_content_str = file_handle.read()
+                starting_pos = file_content_str.find('<title>') + len('<title>')
+                ending_pos = file_content_str.find('</title>')
+                title_from_file = file_content_str[starting_pos:ending_pos]
+                list_of_links = list_of_links + link_for_single_file.format(file_name = name_of_file, file_title = title_from_file)
             
-            newfile = open("home.html").read().format(filename0 = file_list[0], filename1 = file_list[1], filename2 = file_list[2], variable = "")
-            open("home.html", "w").write(newfile)
-            # filename = "filename"
-            # for i in range(len(file_list)):
-            #     filename = filename + str(i)
-            #     print(filename)
-            #     open("home.html").read().format(filename = file_list[i])
-            self.path = "home.html"
-            
+            new_file = template_string.format(variable = list_of_links)
+            open("home.html", "w").write(new_file) 
         
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-    
-
-handler_object = blog_server
-
-PORT = 8000
-my_server = socketserver.TCPServer(("", PORT), handler_object)
 
 
-my_server.serve_forever()
+if __name__ == '__main__':
+    print (f"BLOG SERVER: I am running on http://localhost:{PORT}")
+    my_server = socketserver.TCPServer(("", PORT), blog_server)
+    my_server.serve_forever()
