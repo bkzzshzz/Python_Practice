2db9456ca84ba2b9a3f5e85f5d73e620c1a12451
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-06-30 18:37:22 +0545

    Completetion of blog server

--- python_html_css_blog/blog.py
+++ python_html_css_blog/blog.py
@@ -2,13 +2,16 @@ import http.server
 import socketserver
 import os
 
-PORT = 8001
+PORT = 8000
 class blog_server(http.server.SimpleHTTPRequestHandler):
    
     def do_GET(self):
 
         navbar = open("partials/header.template.html").read()
         body_script = open("partials/scripts.template.html").read()
+        print(f'MY SERVER: The requested path is: {self.path}')
+
+              
         
         if self.path == '/':
             self.path = '/home.html'
@@ -27,10 +30,8 @@ class blog_server(http.server.SimpleHTTPRequestHandler):
                 # Following code extracts title form <title> -------- </title> section of each file
                 file_handle = open("./blog-posts/" + name_of_file)
                 file_content_str = file_handle.read()
-                print(file_content_str)
                 temp_file = file_content_str
                 temp_file = temp_file.lower()
-                print(temp_file)
                 starting_pos = temp_file.find('<title>') + len('<title>')
                 ending_pos = temp_file.find('</title>')
                 title_from_file = file_content_str[starting_pos:ending_pos]
@@ -40,7 +41,7 @@ class blog_server(http.server.SimpleHTTPRequestHandler):
             open("home.html", "w").write(new_file)
         elif self.path == "/book_reviews.html":
             template_string = open("book_reviews.template.html").read().format(header = navbar, scripts = body_script)
-            open("bool_reviews.html", "w").write(template_string)
+            open("book_reviews.html", "w").write(template_string)
         elif self.path == "/gallery.html":
             template_string = open("gallery.template.html").read().format(header = navbar, scripts = body_script)
             open("gallery.html", "w").write(template_string)
@@ -53,8 +54,22 @@ class blog_server(http.server.SimpleHTTPRequestHandler):
         elif self.path == "/my_cv.html":
             template_string = open("my_cv.template.html").read().format(header = navbar, scripts = body_script)
             open("my_cv.html", "w").write(template_string)
+        elif self.path == "/blog-posts/first-post.html":
+            template_string = open("blog-posts/first-post.html").read().format(header = navbar, scripts = body_script)
+            open("non-template-blog-posts/first-post.html", "w").write(template_string)
+            self.path = "non-template-blog-posts/first-post.html"
+        elif self.path == "/blog-posts/my-childhood.html":
+            template_string = open("blog-posts/my-childhood.html").read().format(header = navbar, scripts = body_script)
+            open("non-template-blog-posts/my-childhood.html", "w").write(template_string)
+            self.path = "non-template-blog-posts/my-childhood.html"
+        elif self.path == "/blog-posts/who-is-syd-barret.html":
+            template_string = open("blog-posts/who-is-syd-barret.html").read().format(header = navbar, scripts = body_script)
+            open("non-template-blog-posts/who-is-syd-barret.html", "w").write(template_string)
+            self.path = "non-template-blog-posts/who-is-syd-barret.html"
         else:
             self.path = "404.html"
+        print(f'MY SERVER: The redirected path is: {self.path}')
+
         
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
