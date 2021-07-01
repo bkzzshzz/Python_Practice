7a800f31329e614bd2c5defc0ce609dc472333f4
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-07-01 10:56:31 +0545

    File Management

--- madlibs_game.py
+++
@@ -1,4 +0,0 @@
-noun = input("Enter a noun: ")
-verb = input("Enter a Verb: ")
-print(noun + " " + verb +"s" + " Nepal")
-print(noun + " is proud of being born a Nepali")
--- toggle_cases.py
+++
@@ -1,8 +0,0 @@
-def toggle(character):
-    if character.isupper():
-        return character.lower()
-    if character.islower():
-        return character.upper()
-
-character = input("A Character: ")
-print(toggle(character))
