8d3c70902258ee3b6b04f8b0aed93e36984d2b6e
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-07-29 21:35:06 +0545

    Successful import of the functions from app_logic folder

--- python_html_css_blog/blog.py
+++ python_html_css_blog/blog.py
@@ -7,63 +7,7 @@ import requests
 from requests.sessions import session
 from http import cookies
 from http.cookies import SimpleCookie
-from app_logic import blog_functions
-
-
-def valid_name(name):
-
-    result = ""
-
-
-    for each_letter in name:
-        if each_letter == "+" or each_letter.isalpha():
-            result =  True
-        else:    
-            result = False
-            break
-    
-
-    return result
-
-
-def proper_name(name):
-
-    words_in_name = name.split("+")
-    temp_name = []
-
-
-    for each_word in words_in_name:
-        if each_word != "":
-            temp_name.append(each_word)
-
-
-    words_in_name = temp_name.copy()
-    temp_name = []
-
-
-    for each_word in words_in_name:
-        temp_word = each_word.lower()
-        temp_word = temp_word[0].upper() + temp_word[1:]
-        temp_name.append(temp_word)
-
-
-    words_in_name = temp_name.copy()
-
-    return " ".join(words_in_name)
-
-
-def email_okay(email, variable):
-
-    if email not in variable:
-        return True
-
-
-def password_match(password, re_password):
-
-    if password == re_password:
-        return True
-    else:
-        return False
+from app_logic.blog_functions import valid_name, proper_name, password_match, email_okay
 
 
 PORT = random.randrange(8000, 8005)
